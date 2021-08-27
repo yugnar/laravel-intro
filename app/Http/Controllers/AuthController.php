69b9811937e2a71688ec5b66ea3ee3528ad18f07
Aldo Ortiz <aortizm@live.com.mx>
2021-08-26 21:43:35 -0500

    Corrección de error Account
    Se agrega la línea use App\Models\Account; en AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Account;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
